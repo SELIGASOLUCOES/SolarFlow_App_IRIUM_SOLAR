4408b2fe391eef25d355ac3feae081e9ab5cd3c0
SELIGASOLUCOES <marcosmarcaocruz@gmail.com>
2025-11-21 13:56:57 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "solarflow-v13-github";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+  "https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
